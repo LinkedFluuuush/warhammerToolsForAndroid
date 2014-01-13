e7bd16b6e78cb9d63e1073ca9ee3916f40a67ffb
LinkedFluuuush <linkedfluuuush@gmail.com>
2014-01-14 00:57:29 +0100

    Layout advance

--- src/com/linkedFluuuush/warhammerToolsForAndroid/fragment/DisplayFragment.java
+++ src/com/linkedFluuuush/warhammerToolsForAndroid/fragment/DisplayFragment.java
@@ -11,7 +11,9 @@ import com.linkedFluuuush.warhammerToolsForAndroid.core.World;
 import com.linkedFluuuush.warhammerToolsForAndroid.core.characteristics.Career;
 import com.linkedFluuuush.warhammerToolsForAndroid.core.characteristics.Race;
 import com.linkedFluuuush.warhammerToolsForAndroid.core.entities.Character;
+import com.linkedFluuuush.warhammerToolsForAndroid.core.entities.God;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -31,6 +33,18 @@ public class DisplayFragment extends Fragment {
     Character character;
 
     EditText textName;
+    EditText textAge;
+    EditText textEyeColor;
+    EditText textHairColor;
+    EditText textAstralSign;
+    EditText textBirthPlace;
+    EditText textHeight;
+    EditText textWeight;
+    EditText textDistinguishingSigns;
+
+    Spinner genderSpinner;
+    Spinner mainHandSpinner;
+    Spinner godSpinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedIntanceState){
@@ -46,21 +60,57 @@ public class DisplayFragment extends Fragment {
         careerSpinner = (Spinner) rootView.findViewById(R.id.careerSpinner);
         buttonCreate = (Button) rootView.findViewById(R.id.buttonCreate);
         characterText = (TextView) rootView.findViewById(R.id.characterText);
+
         textName = (EditText) rootView.findViewById(R.id.textName);
+        textAge = (EditText) rootView.findViewById(R.id.textAge);
+        textEyeColor = (EditText) rootView.findViewById(R.id.textEyeColour);
+        textHairColor = (EditText) rootView.findViewById(R.id.textHairColour);
+        textAstralSign = (EditText) rootView.findViewById(R.id.textAstralSign);
+        textBirthPlace = (EditText) rootView.findViewById(R.id.textBirthPlace);
+        textHeight = (EditText) rootView.findViewById(R.id.textHeight);
+        textWeight = (EditText) rootView.findViewById(R.id.textWeight);
+        textDistinguishingSigns = (EditText) rootView.findViewById(R.id.textDistinguishingSigns);
 
-        List<Career> careerList = World.CAREERS;
+        genderSpinner = (Spinner) rootView.findViewById(R.id.spinnerGender);
+        mainHandSpinner = (Spinner) rootView.findViewById(R.id.spinnerMainHand);
+        godSpinner = (Spinner) rootView.findViewById(R.id.favoriteGod);
 
-        ArrayAdapter<Career> careerDataAdapter = new ArrayAdapter<Career>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, careerList);
+        ArrayAdapter<Career> careerDataAdapter = new ArrayAdapter<Career>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, World.CAREERS);
         careerDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         careerSpinner.setAdapter(careerDataAdapter);
 
-        List<Race> raceList = World.RACES;
-
-        ArrayAdapter<Race> raceDataAdapter = new ArrayAdapter<Race>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, raceList);
+        ArrayAdapter<Race> raceDataAdapter = new ArrayAdapter<Race>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, World.RACES);
         raceDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         raceSpinner.setAdapter(raceDataAdapter);
+
+        ArrayAdapter<God> godDataAdapter = new ArrayAdapter<God>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, World.GODS);
+        godDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        godSpinner.setAdapter(godDataAdapter);
+
+        List<String> genderList = new LinkedList<String>();
+        genderList.add("Masculin");
+        genderList.add("Féminin");
+        genderList.add("Indéfini");
+
+        ArrayAdapter<String> genderDataAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, genderList);
+        genderDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        genderSpinner.setAdapter(genderDataAdapter);
+
+        List<String> handList = new LinkedList<String>();
+        handList.add("Droite");
+        handList.add("Gauche");
+        handList.add("Ambidextre");
+        handList.add("Ambisinistre ");
+
+        ArrayAdapter<String> handDataAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, handList);
+        handDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        mainHandSpinner.setAdapter(handDataAdapter);
+
 
         if(character != null){
             characterText.setText(character.toString());
@@ -74,9 +124,36 @@ public class DisplayFragment extends Fragment {
 
                 character = new Character(race, career);
 
-                characterText.setText(character.toString());
+                //characterText.setText(character.toString());
 
                 textName.setText(character.getName());
+                textAge.setText(character.getDetails().getAge() + " ans");
+                textEyeColor.setText(character.getDetails().getEyeColour());
+                textHairColor.setText(character.getDetails().getHairColour());
+                textAstralSign.setText(character.getDetails().getAstralSign().getName());
+                if(((character.getDetails().getHeight() % 100) + "").length() == 1)
+                    textHeight.setText(character.getDetails().getHeight() / 100 + "m0" + character.getDetails().getHeight() % 100);
+                else
+                    textHeight.setText(character.getDetails().getHeight() / 100 + "m" + character.getDetails().getHeight() % 100);
+                textWeight.setText(character.getDetails().getWeight() + " kg");
+                textBirthPlace.setText(character.getDetails().getBirthplace());
+
+                String s = "";
+                for(String sign : character.getDetails().getDistinguishingMarks()){
+                    s += sign + "\n";
+                }
+                s = s.substring(0, s.length() - 1);
+                textDistinguishingSigns.setText(s);
+
+                if(character.getDetails().isMale()){
+                    genderSpinner.setSelection(0);
+                } else {
+                    genderSpinner.setSelection(1);
+                }
+
+                godSpinner.setSelection(World.GODS.indexOf(character.getDetails().getFavoriteGod()));
+
+                Toast.makeText(getActivity().getApplicationContext(), character.getDetails().toString(), Toast.LENGTH_LONG).show();
             }
         });
 
